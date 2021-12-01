e48842116a2393ccf1d288cd52f2f55dd31fb49f
Rumen Guin <rumen2k@gmail.com>
2021-12-01 18:57:31 +0530

    Initial Commit

---
+++ EcommerceAppKit/EcommerceAppKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppKitApp.swift
+//  EcommerceAppKit
+//
+//  Created by RUMEN GUIN on 01/12/21.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
